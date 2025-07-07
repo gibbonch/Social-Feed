797a52278e4530a491bd153c1c3138a904434e00
gibbonch <toropov280304@icloud.com>
2025-07-07 15:09:47 +0300

    setup project

---
+++ SocialFeed/Application/SceneDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = assembly()
+        window?.makeKeyAndVisible()
+    }
+    
+    private func assembly() -> UIViewController {
+        return FeedViewController()
+    }
+
+}
---
+++ SocialFeed/Scenes/Feed/View/FeedViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
