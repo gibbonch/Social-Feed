abb9d4ce356370f107aadf46033c85c4b255491f
gibbonch <toropov280304@icloud.com>
2025-07-07 10:23:43 +0300

    Initial Commit

---
+++ SocialFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialFeed
+//
+//  Created by Александр Торопов on 07.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
